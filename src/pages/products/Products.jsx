2fb63a3ca5c10c957e096bd68aa9467650b1b762
Pedrowill21 <pedrowilliam2999@gmail.com>
2023-09-21 12:43:20 -0300

    products

--- src/pages/products/Products.jsx
+++ src/pages/products/Products.jsx
@@ -8,9 +8,39 @@ const Products = () => {
 
   return (
    <div className={styles.container}>
-    <div>
+    <div className={styles.text}>
       <h2>Conheça as cápsulas CAPPUCCINO® Farmers Origins</h2>
       <p>Vindas de cinco das melhores terras cafeicultoras do planeta, CAPPUCCINO® Farmers Origins chegaram para ampliar horizontes em seus sentidos.</p>
+    </div>
+
+    <div className={styles.products}>
+      {data.map((product)=>{
+        return (
+          <div className={styles.card} key={product.id}>
+        <div className={styles.media}>
+          <img src={product.img}/>
+        </div>
+        <div className={styles.info}>
+            <h2>{product.title}</h2>
+        </div>
+      </div>
+        )
+      })}
+    </div>
+
+    <div className={styles.products}>
+      {data.map((product)=>{
+        return (
+          <div className={styles.card} key={product.id}>
+        <div className={styles.media}>
+          <img src={product.img}/>
+        </div>
+        <div className={styles.info}>
+            <h2>{product.title}</h2>
+        </div>
+      </div>
+        )
+      })}
     </div>
 
     <div className={styles.products}>
